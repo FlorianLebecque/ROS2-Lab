91f43cc5f4696f2ffdcc57e05754d09ead3d6989
FlorianLebecque <florianlebecque@hotmail.com>
2024-04-18 17:21:08 +0200

    display node data into canvas

--- src/components/topics/Display/imu_data/imu_data.tsx
+++ src/components/topics/Display/imu_data/imu_data.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber'; // Hook for animation
 import ImuDataDisplayMesh from './imu_mesh';
+import ImuDataDisplayText from './imu_text';
 
 export default function ImuDataDisplay(props: { name: string }) {
 
@@ -10,6 +11,8 @@ export default function ImuDataDisplay(props: { name: string }) {
             <directionalLight position={[0, 3, 2]} intensity={1} castShadow />
 
             <ImuDataDisplayMesh name={props.name} />
+            <ImuDataDisplayText name={props.name} />
+
         </Canvas>
     );
 }
