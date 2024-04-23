dae01446d21cacfb0fb3129c3d421a776bcb13de
FlorianLebecque <florianlebecque@hotmail.com>
2024-04-23 17:13:15 +0200

    scroll to bottom of simple data topic

--- src/components/topics/Display/basicDisplay/basicDisplay.tsx
+++ src/components/topics/Display/basicDisplay/basicDisplay.tsx
@@ -1,25 +1,36 @@
 'use client';
 
 import { useData } from "@/components/topics/topicProvider/topicProvider";
+import { useEffect, useRef } from "react";
 
 
 export default function BasicDisplay(props: { name: string }) {
 
     const { data, setData } = useData();
 
+    const messagesEndRef = useRef<null | HTMLDivElement>(null)
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+
+    useEffect(() => {
+        scrollToBottom()
+    }, [data]);
+
+
     return (
         <div style={{ height: "100%", overflowY: "auto" }}>
-            <div>
-                {data.map((message, index) => {
-                    return (
-                        <div key={index}>
-                            <p>{message.time}</p>
-                            <hr />
-                            <pre>{JSON.stringify(message.ros, null, 2)}</pre>
-                        </div>
-                    );
-                })}
-            </div>
+            {data.map((message, index) => {
+                return (
+                    <div key={index}>
+                        <p>{message.time}</p>
+                        <hr />
+                        <pre>{JSON.stringify(message.ros, null, 2)}</pre>
+                    </div>
+                );
+            })}
+            <div ref={messagesEndRef} />
         </div >
     );
 }
