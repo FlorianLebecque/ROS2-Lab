6c5c70f0fbaea766c45fc27419db4656ccbe168b
FlorianLebecque <florianlebecque@hotmail.com>
2024-04-25 18:05:48 +0200

    clear

--- src/components/robots/robot_card/robot_cart.tsx
+++ src/components/robots/robot_card/robot_cart.tsx
@@ -1,35 +1,9 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { useRosWeb } from '@/components/RosContext';
 import Link from 'next/link';
 
 export default function RobotCart(props: { name: string }) {
     const { name } = props;
-    // const rosWeb = useRosWeb();
-
-    // const [nodeDetails, setNodeDetails] = useState({ subscribers: [], topics: [], services: [] });
-
-    // useEffect(() => {
-    //     const fetchNodeDetails = async () => {
-    //         try {
-    //             const details = await rosWeb.GetNodeDetails(name);
-    //             setNodeDetails(details);
-    //         } catch (error) {
-    //             console.error("Error fetching node details:", error);
-    //         }
-    //     };
-
-    //     fetchNodeDetails();
-
-    //     setInterval(() => {
-    //         fetchNodeDetails();
-    //     }, 2000);
-
-    // }, [name, rosWeb]);
-
-    // const address = name.replace(/\/+/g, '@');
-
-    // ... rest of the component logic
 
     return (
         <div className="card shade" style={{ width: '18rem' }}>
