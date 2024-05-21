f4214bdb4041d2bfd56c6eb6ee22d1c64cb7ef90
emile le flecher <emile.leflecher@mil.be>
2024-05-21 14:36:56 +0200

    can use display based on topic name

--- src/components/topics/topicVisualizerFactory.tsx
+++ src/components/topics/topicVisualizerFactory.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import BasicDisplay from '@/components/topics/Display/basicDisplay/basicDisplay';
 import ImuDataDisplay from './Display/imu_data/imu_data';
-import{VideoDisplayJPEG, VideoDisplayUncompressed} from './Display/videoDisplay/videoDisplay';
+import { VideoDisplayJPEG, VideoDisplayUncompressed } from './Display/videoDisplay/videoDisplay';
+import ThreadHandleDisplay from './Display/ime/thread_handle';
 
 interface ComponentFactoryProps {
     topicName: string;
@@ -10,8 +11,19 @@ interface ComponentFactoryProps {
 
 const TopicVisualizerFactory = ({ topicName, topicType }: ComponentFactoryProps) => {
 
-    // Define the components that can be displayed 
-    const components: { [key: string]: JSX.Element } = {
+    const componentsByName: { [key: string]: JSX.Element } = {
+        'ime_thread_handle': (<ThreadHandleDisplay name={topicName} />),
+    };
+
+    // If the topic name is partially matched, return the component, if not check with the topic type
+    for (const key in componentsByName) {
+        if (topicName.includes(key)) {
+            return componentsByName[key];
+        }
+    }
+
+    // Define the components that can be displayed using the topic Type
+    const componentsByType: { [key: string]: JSX.Element } = {
         'sensor_msgs/msg/Imu': (<ImuDataDisplay name={topicName} />),
         'sensor_msgs/msg/CompressedImage': (<VideoDisplayJPEG name={topicName} />),
         'sensor_msgs/msg/Image': (<VideoDisplayUncompressed name={topicName} />),
@@ -19,9 +31,9 @@ const TopicVisualizerFactory = ({ topicName, topicType }: ComponentFactoryProps)
     };
 
 
-    if (!components[topicType]) return components['default'];
+    if (!componentsByType[topicType]) return componentsByType['default'];
 
-    return components[topicType];
+    return componentsByType[topicType];
 };
 
 export default TopicVisualizerFactory;
