d1f02337fe3f91362a94c17c4345388f38cfa378
FlorianLebecque <florianlebecque@hotmail.com>
2024-05-27 16:06:47 +0200

    Use topic data to display marker

--- src/components/topics/display/gps/RobotMarker.tsx
+++ src/components/topics/display/gps/RobotMarker.tsx
@@ -1,41 +1,55 @@
 import { MapContainer, Marker, Popup, TileLayer, Tooltip, useMap } from "react-leaflet"
 
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import L, { LatLng } from "leaflet";
 import Link from "next/link";
+import { useData } from "../../../topics/topicProvider";
 
 export default function RobotMarker(props: { robot: string }) {
 
     // use map
     const map = useMap();
 
-    const [location, setLocation] = useState([50.843941, 4.3930369]);
+    const { data, setData, setPause, pause } = useData();
+
+    //const [location, setLocation] = useState([50.843941, 4.3930369]);
 
     useEffect(() => {
-        const interval = setInterval(() => {
 
-            let dx = Math.random() * 0.00001;
-            let dy = Math.random() * 0.00001;
-            let dir_x = Math.random() > 0.5 ? 1 : -1;
-            let dir_y = Math.random() > 0.5 ? 1 : -1;
+        let location = [0, 0];
 
-            location[0] += dx * dir_x;
-            location[1] += dy * dir_y;
+        if (data.length > 0) {
+            const lastData = data[data.length - 1].ros;
+            location = [lastData.latitude, lastData.longitude];
+        }
 
-            setLocation([location[0], location[1]]);
+        if (location[0] > map.getBounds().getNorth() || location[0] < map.getBounds().getSouth() || location[1] > map.getBounds().getEast() || location[1] < map.getBounds().getWest()) {
+            map.panTo(new LatLng(location[0], location[1]), { animate: true, duration: 1 });
+        }
 
-            // move the map if the robot is out of the view
-            if (location[0] > map.getBounds().getNorth() || location[0] < map.getBounds().getSouth() || location[1] > map.getBounds().getEast() || location[1] < map.getBounds().getWest()) {
-                map.panTo(new LatLng(location[0], location[1]), { animate: true, duration: 1 });
-            }
+        // const interval = setInterval(() => {
+
+        //     let dx = Math.random() * 0.00001;
+        //     let dy = Math.random() * 0.00001;
+        //     let dir_x = Math.random() > 0.5 ? 1 : -1;
+        //     let dir_y = Math.random() > 0.5 ? 1 : -1;
+
+        //     location[0] += dx * dir_x;
+        //     location[1] += dy * dir_y;
+
+        //     setLocation([location[0], location[1]]);
+
+        //     // move the map if the robot is out of the view
+        //     
 
 
-        }, 200);
-        return () => clearInterval(interval);
-    });
+        // }, 200);
+        // return () => clearInterval(interval);
+
+    }, [data]);
 
     return (
-        <Marker icon={L.icon({ iconUrl: "https://api.dicebear.com/8.x/bottts/svg?seed=" + props.robot, iconSize: [30, 30] })} position={new LatLng(location[0], location[1])}>
+        <Marker icon={L.icon({ iconUrl: "https://api.dicebear.com/8.x/bottts/svg?seed=" + props.robot, iconSize: [30, 30] })} position={new LatLng(0, 0)} >
             <Popup>
                 <Link className="btn btn-primary" href={"/" + props.robot}> {props.robot} </Link>
             </Popup>
--- src/components/topics/display/gps/rtkmsg.tsx
+++ src/components/topics/display/gps/rtkmsg.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet"
+import { MapContainer, Marker, Popup, TileLayer, Tooltip, useMapEvents } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
-import L from "leaflet";
+import L, { LeafletMouseEvent } from "leaflet";
 import { use, useEffect, useState } from "react";
 import { LatLng } from "leaflet";
 import RobotMarker from "./RobotMarker";
@@ -10,9 +10,21 @@ import RobotMarker from "./RobotMarker";
 // import "leaflet-defaulticon-compatibility"
 // import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 
+function MapClickHandler() {
+    const map = useMapEvents({
+        click: (ev: LeafletMouseEvent) => {
+            console.log('map clicked:', ev.latlng);
+        }
+    })
+    return null
+}
+
+
 export default function GPSMap(props: { robot: string }) {
 
     return <MapContainer center={[50.843941, 4.3930369]} zoom={20} scrollWheelZoom={true}>
+        <MapClickHandler />
+
         <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
