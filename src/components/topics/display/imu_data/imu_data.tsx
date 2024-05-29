4aa9a2dc5a089bd83d231fa8bdeeff0955423b08
Florian Lebecque <florianlebecque@hotmail.com>
2024-05-29 13:37:51 +0200

    improve visual and controls

--- src/components/topics/display/imu_data/imu_data.tsx
+++ src/components/topics/display/imu_data/imu_data.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber'; // Hook for animation
 import ImuDataDisplayMesh from './imu_mesh';
-import ImuDataDisplayText from './imu_text';
+import { OrbitControls } from '@react-three/drei';
 
 export default function ImuDataDisplay(props: { name: string }) {
 
@@ -10,6 +10,8 @@ export default function ImuDataDisplay(props: { name: string }) {
             <ambientLight intensity={Math.PI / 2} />
             <directionalLight position={[0, 3, 2]} intensity={1} castShadow />
             <ImuDataDisplayMesh name={props.name} />
+            <gridHelper args={[10, 10]} position-y={-0.25} />
+            <OrbitControls />
         </Canvas>
     );
 }
