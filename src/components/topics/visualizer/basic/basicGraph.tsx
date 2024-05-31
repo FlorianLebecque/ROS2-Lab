47322d7905c9fdb1f3e6694349ae78fbf7ef1357
Florian Lebecque <florianlebecque@hotmail.com>
2024-05-31 16:26:25 +0200

    Add missing parameters

--- src/components/topics/visualizer/basic/basicGraph.tsx
+++ src/components/topics/visualizer/basic/basicGraph.tsx
@@ -1,11 +1,11 @@
 import BasicGraph from "../../display/basicDisplay/basicGraph";
 import { TopicProvider } from "../../topicProvider";
 
-export default function BasicGraphVisualizer(props: { topic: string, type: string, dataset: string, title: string, min: number, max: number }) {
+export default function BasicGraphVisualizer(props: { topic: string, type: string, dataset: string, title: string, min: number, max: number, single: boolean }) {
 
     return (
         <TopicProvider topicName={props.topic} >
-            <BasicGraph name={props.topic} dataset={props.dataset} title={props.title} min={props.min} max={props.max} />
+            <BasicGraph name={props.topic} dataset={props.dataset} title={props.title} min={props.min} max={props.max} single={props.single} />
         </TopicProvider>
     );
 }
