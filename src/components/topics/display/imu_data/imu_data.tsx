2192fe8ce674ed31a41f2d41687ae0c1fbfbe4ec
Florian Lebecque <florianlebecque@hotmail.com>
2024-06-04 17:51:36 +0200

    Add origin frame

--- src/components/topics/display/imu_data/imu_data.tsx
+++ src/components/topics/display/imu_data/imu_data.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber'; // Hook for animation
 import ImuDataDisplayMesh from './imu_mesh';
 import { OrbitControls } from '@react-three/drei';
+import OriginMesh from '@/utils/meshes/origin';
 
 export default function ImuDataDisplay(props: { name: string }) {
 
@@ -12,6 +13,8 @@ export default function ImuDataDisplay(props: { name: string }) {
             <ImuDataDisplayMesh name={props.name} />
             <gridHelper args={[10, 10]} position-y={-0.25} />
             <OrbitControls />
+            <OriginMesh />
+
         </Canvas>
     );
 }
--- src/components/topics/display/odom/odom.tsx
+++ src/components/topics/display/odom/odom.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber'; // Hook for animation
 import { OrbitControls } from '@react-three/drei';
 import OdomDataDisplayMesh from './odom_mesh';
+import OriginMesh from '@/utils/meshes/origin';
 
 export default function OdomDisplay(props: { name: string }) {
 
@@ -13,7 +14,8 @@ export default function OdomDisplay(props: { name: string }) {
             <directionalLight position={[0, 3, 2]} intensity={1} castShadow />
             <OdomDataDisplayMesh OrbitRef={OrbitControl} name={props.name} />
             <gridHelper args={[100, 100]} position-y={-0.25} />
-            <OrbitControls ref={OrbitControl} />
+            <OrbitControls ref={OrbitControl} target={[0, 0, 0]} position={[-5, 5, 0]} />
+            <OriginMesh />
         </Canvas>
     );
 }
