c9f50b50c71745495a74dd1a0f278d2c2a7b902e
FlorianLebecque <florianlebecque@hotmail.com>
2024-06-13 17:06:02 +0200

    change robot ui

--- webapp/src/components/Robots/RobotCart/RobotCart.tsx
+++ webapp/src/components/Robots/RobotCart/RobotCart.tsx
@@ -2,15 +2,19 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 
+import Style from './RobotCart.module.css';
+
 export default function RobotCart(props: { name: string }) {
     const { name } = props;
 
     return (
-        <div className="card shade" style={{ width: '18rem' }}>
-            <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <Link href={"/" + name} className="btn btn-primary">Navigate</Link>
-            </div>
+        <div className={"card shade " + Style.RobotCart} style={{ width: '18rem' }}>
+            <Link href={"/" + name} className="">
+                <img src={"https://api.dicebear.com/9.x/bottts-neutral/svg?seed=" + name} />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                </div>
+            </Link>
         </div>
     );
 }
