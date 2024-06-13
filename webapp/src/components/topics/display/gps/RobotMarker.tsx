95c8f0eac9a7639f668dd4d136ce3efe1b2178f2
FlorianLebecque <florianlebecque@hotmail.com>
2024-06-13 16:03:02 +0200

    clean

--- webapp/src/components/topics/display/gps/RobotMarker.tsx
+++ webapp/src/components/topics/display/gps/RobotMarker.tsx
@@ -12,44 +12,23 @@ export default function RobotMarker(props: { robot: string }) {
 
     const { data, setData, setPause, pause } = useData();
 
-    //const [location, setLocation] = useState([50.843941, 4.3930369]);
+    const [location, setLocation] = useState([50.843941, 4.3930369]);
 
     useEffect(() => {
 
-        let location = [0, 0];
-
         if (data.length > 0) {
             const lastData = data[data.length - 1].ros;
-            location = [lastData.latitude, lastData.longitude];
+            setLocation([lastData.latitude, lastData.longitude]);
         }
 
         if (location[0] > map.getBounds().getNorth() || location[0] < map.getBounds().getSouth() || location[1] > map.getBounds().getEast() || location[1] < map.getBounds().getWest()) {
             map.panTo(new LatLng(location[0], location[1]), { animate: true, duration: 1 });
         }
 
-        // const interval = setInterval(() => {
-
-        //     let dx = Math.random() * 0.00001;
-        //     let dy = Math.random() * 0.00001;
-        //     let dir_x = Math.random() > 0.5 ? 1 : -1;
-        //     let dir_y = Math.random() > 0.5 ? 1 : -1;
-
-        //     location[0] += dx * dir_x;
-        //     location[1] += dy * dir_y;
-
-        //     setLocation([location[0], location[1]]);
-
-        //     // move the map if the robot is out of the view
-        //     
-
-
-        // }, 200);
-        // return () => clearInterval(interval);
-
     }, [data]);
 
     return (
-        <Marker icon={L.icon({ iconUrl: "https://api.dicebear.com/8.x/bottts/svg?seed=" + props.robot, iconSize: [30, 30] })} position={new LatLng(0, 0)} >
+        <Marker icon={L.icon({ iconUrl: "https://api.dicebear.com/8.x/bottts/svg?seed=" + props.robot, iconSize: [30, 30] })} position={new LatLng(location[0], location[1])} >
             <Popup>
                 <Link className="btn btn-primary" href={"/" + props.robot}> {props.robot} </Link>
             </Popup>
