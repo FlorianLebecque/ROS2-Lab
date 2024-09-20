29c6b48968da5be1e8c47b7ab4b5a48c718f990c
FlorianLebecque <florianlebecque@hotmail.com>
2024-09-20 13:15:57 +0200

    Add auto colors

--- webapp/src/components/Topics/visualizer/number/multiTopicGraph.tsx
+++ webapp/src/components/Topics/visualizer/number/multiTopicGraph.tsx
@@ -1,6 +1,21 @@
 import { MultiTopicProvider } from "../../multiTopicProvider";
 import MultiGraph from "./private/multiGraph";
 
+const generatePastelColorFromTopicName = (topic: string) => {
+
+    let hash = 0;
+    for (let i = 0; i < topic.length; i++) {
+        hash = topic.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    const c = (hash & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+
+    return "#" + "00000".substring(0, 6 - c.length) + c;
+
+}
+
 export default function MultiTopicGraph(props: { namespaceExtractor: string, topic: string, type: string, colors: Map<string, string>, topics: Map<string, string> }) {
 
 
@@ -20,6 +35,11 @@ export default function MultiTopicGraph(props: { namespaceExtractor: string, top
     // convert props.topics to a map
     let topics_map = new Map<string, string>(Object.entries(props.topics));
 
+    // if topics_map is empty, add the topic
+    if (topics_map.size === 0) {
+        topics_map.set(props.topic, props.topic);
+    }
+
     for (let [key, value] of topics_map) {
         if (namespace !== "") {
             // check if the topic has already a first '/' if not add it
@@ -30,7 +50,14 @@ export default function MultiTopicGraph(props: { namespaceExtractor: string, top
         }
     }
 
-    const colors_map = new Map<string, string>(Object.entries(props.colors));
+    let colors_map = new Map<string, string>(Object.entries(props.colors));
+
+    // add color to the topic that is not in the map
+    for (let [key, value] of topics_map) {
+        if (!colors_map.has(key)) {
+            colors_map.set(key, generatePastelColorFromTopicName(value));
+        }
+    }
 
     return (
         <MultiTopicProvider topicsNames={topics_map} >
